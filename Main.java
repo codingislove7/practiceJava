088ae483dee815b7a86358d22535a2b8a13dc0e5
sssorsss <sssorsss@pop-os.localdomain>
2021-01-24 02:31:15 +0330

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main (String[] args) {
+        byte age = 26;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+        System.out.println(age);
+    }
+}
+ 
